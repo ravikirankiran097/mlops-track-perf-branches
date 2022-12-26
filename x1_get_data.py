dd77d57bd0b2b9297efbfe03a72dad115d472c3d
ravikirankiran097 <55925733+ravikirankiran097@users.noreply.github.com>
2022-12-26 13:12:21 +0530

    Update x1_get_data.py

--- x1_get_data.py
+++ x1_get_data.py
@@ -1,9 +1,16 @@
 import os
 import wget
 import zipfile
+from urllib.request import Request, urlopen
 
 # Download the zipped dataset
-url = 'https://storage.googleapis.com/trainingdata-mlops/data.zip'
+#url = 'https://storage.googleapis.com/trainingdata-mlops/data.zip'
+req = Request(
+    url='https://storage.googleapis.com/trainingdata-mlops/data.zip', 
+    headers={'User-Agent': 'Mozilla/5.0'}
+)
+webpage = urlopen(req).read()
+
 zip_name = "data.zip"
 wget.download(url, zip_name)
 
